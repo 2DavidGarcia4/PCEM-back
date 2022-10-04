f1b7dc3ac74fa344351d4d6ff7d482c0562aac3d
2DavidGarcia4 <gacub20ru@gmail.com>
2022-10-03 21:16:30 -0500

    add cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import "./db.js"
 import express from "express"
 import { router } from "./routes/pcem.routes.js"
+import cors from "cors"
 
 export const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.use(router)
